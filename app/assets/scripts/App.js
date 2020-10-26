7e21d1636dc862df8584e38a0395fc5393889558
Trent <overlooked723@gmail.com>
2020-10-26 21:22:43 +1100

    Section 12 and 13: Header styling and JS example

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,14 @@
 import '../styles/styles.css'
+import Person from './modules/Person'
 
 if (module.hot) {
     module.hot.accept()
 }
+
+/* Lesson example code below this line */
+
+let john = new Person("John Doe", "purple");
+john.greet();
+
+let jane = new Person("Jane Smith", "green");
+jane.greet();
